91b24c48dbfe299d3fc66be66c78be17c44ba11e
Simone Marino <simo.marino@outlook.it>
2023-06-23 15:11:28 +0200

    Added show view

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/PizzaController.java
@@ -3,12 +3,16 @@ package org.lessons.java.springlamiapizzeriacrud.controller;
 import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.java.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -18,12 +22,45 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model) { // Model è la mappa di attributi che il controller passa alla view
-        // deve recuperare la lista di pizze dal DB
+    public String index(Model model) {
+        // Recupera la lista di pizze dal DB
         List<Pizza> pizzas = pizzaRepository.findAll();
         //Passo la lista delle pizze alla view attraverso il model
         model.addAttribute("pizzaList", pizzas);
-        // Ritorno il nome del template della view
-        return "/pizzas/index";
+        return "/pizzas/index"; // ritorno la vista index
+    }
+
+    // Pagina di dettaglio
+
+    // 1° METODO: Il metodo getReferenceById potrebbe non ritornare nulla e va gestito con un try/catch
+
+    /*@GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        try {
+            // Recupero l' ID
+            Pizza pizza = pizzaRepository.getReferenceById(id);
+            // Aggiungo l'oggetto "pizza" all'oggetto "model" per renderlo disponibile nella vista
+            model.addAttribute("pizza", pizza);
+        } catch (EntityNotFoundException e) {
+            return "redirect:/pizzas";
+        }
+        return "pizzas/show"; // ritorno la vista show
+    }*/
+
+    // 2° METODO: Il metodo findById prevede che io non possa trovare l' ID, di conseguenza mi restituisce un Optional
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer pizzaId, Model model) {
+        // cerca su database i dettagli della pizza con quell'id
+        Optional<Pizza> result = pizzaRepository.findById(pizzaId);
+        if (result.isPresent()) {
+            // passa la pizza alla view
+            model.addAttribute("pizza", result.get());
+            // ritorna il nome del template della view
+            return "/pizzas/show";
+        } else {
+            // ritorno un HTTP Status 404 Not Found
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Book with id " + pizzaId + " not found");
+        }
     }
 }
