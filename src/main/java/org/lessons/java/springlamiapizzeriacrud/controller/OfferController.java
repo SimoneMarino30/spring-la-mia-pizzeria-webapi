66948105489e587cb38c4d59783ae703f1d294cd
Simone Marino <simo.marino@outlook.it>
2023-06-29 20:48:11 +0200

    Model Offer + OfferController

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/OfferController.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/OfferController.java
@@ -1,4 +1,88 @@
 package org.lessons.java.springlamiapizzeriacrud.controller;
 
+import jakarta.validation.Valid;
+import org.lessons.java.springlamiapizzeriacrud.model.Offer;
+import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
+import org.lessons.java.springlamiapizzeriacrud.repository.OfferRepository;
+import org.lessons.java.springlamiapizzeriacrud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/offers")
 public class OfferController {
+
+    @Autowired
+    OfferRepository offerRepository;
+    @Autowired
+    PizzaRepository pizzaRepository;
+
+    @GetMapping("/create")
+    public String create(@RequestParam("pizzaId") Integer pizzaId, Model model) {
+        Optional<Pizza> foundPizza = pizzaRepository.findById(pizzaId);
+        if (foundPizza.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        Offer offer = new Offer();
+        offer.setStartDate(LocalDate.now());
+        offer.setEndDate(LocalDate.now().plusDays(1));
+        offer.setPizza(foundPizza.get());
+        model.addAttribute("offer", offer);
+        return "offers/form";
+
+    }
+
+    @PostMapping("/create")
+    public String doCreate(@Valid @ModelAttribute("offer") Offer formOffer,
+                           BindingResult bindingResult) {
+        if (formOffer.getEndDate().isBefore(formOffer.getStartDate()))
+            bindingResult.addError(new FieldError("offer", "endDate", formOffer.getEndDate(), false, null, null, "The end date of the offer must be later than the start date"));
+        if (bindingResult.hasErrors()) return "/offers/form";
+        offerRepository.save(formOffer);
+        // redirect
+        return "redirect:/pizzas";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Optional<Offer> offer = offerRepository.findById(id);
+        if (offer.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        // invio offer al model
+        model.addAttribute("offer", offer.get());
+        return "/offers/form";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(
+            @PathVariable Integer id,
+            @Valid @ModelAttribute("offer") Offer offer,
+            BindingResult bindingResult) {
+        // verifico che esiste
+        Optional<Offer> foundOffer = offerRepository.findById(id);
+        if (foundOffer.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        if (offer.getEndDate().isBefore(offer.getStartDate()))
+            bindingResult.addError(new FieldError("offer", "endDate", offer.getEndDate(), false, null, null, "The end date of the offer must be later than the start date"));
+        if (bindingResult.hasErrors()) return "/offers/form";
+        // setto l'id
+        offer.setId(id);
+        offerRepository.save(offer);
+        return "redirect:/pizzas";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        Optional<Offer> foundOffer = offerRepository.findById(id);
+        if (foundOffer.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        offerRepository.delete(foundOffer.get());
+        return "redirect:/pizzas";
+    }
+
+
 }
--- src/main/java/org/lessons/java/springlamiapizzeriacrud/model/Offer.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/model/Offer.java
@@ -1,4 +1,69 @@
 package org.lessons.java.springlamiapizzeriacrud.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+@Entity
 public class Offer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotBlank
+    private String title;
+    @NotNull
+    @FutureOrPresent
+    //viene utilizzata per specificare che un determinato elementodeve rappresentare una data futura o la data corrente. Se viene violata la regola definita dall'annotazione, verrà sollevata un'eccezione di validazione.
+    private LocalDate startDate;
+    @NotNull
+    @FutureOrPresent
+    private LocalDate endDate;
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pizza pizza;
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    // GETTERS & SETTERS
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
 }
