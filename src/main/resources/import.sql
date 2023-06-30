530088a42eaf4db84c026819e4be447be426cffa
Simone Marino <simo.marino@outlook.it>
2023-06-30 17:55:35 +0200

    Populated pivot-table pizza_ingredient on DB

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,17 @@
+<!-- PIZZE
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (5.99, 'An irresistible symphony of colors, shapes, and aromas harmoniously blend on the crispy round surface. A culinary masterpiece that captivates both palate and heart.', 'Margherita', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/dzkieoorh0hh2ktj8ob5', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (6.99, 'An enchanting blend of vibrant flavors dances atop the thin, crusty canvas. A culinary masterpiece that transports you to the sun-kissed shores of Italy.', 'Marinara Strange', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/gkqt2nhoeejvalrdjdxo', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (7.99, 'A heavenly marriage of creamy buffalo mozzarella and a delicate dough, crowned with exquisite flavors. A gastronomic delight that will transport you to pizza paradise', 'Bufalina', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/w0jq0uuu2pzl4jkory49', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES	(10.99, 'A symphony of flavors emerges from the combination of juicy San Marzano tomatoes, creamy Agerola fior di latte, and delicate anchovies. Napoli\'s culinary treasure, captured in every bite.', 'Napoli', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/rhi77c98l26rxamk4mju', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (11.99, 'A fiery flavor journey awaits with Molise Pizza: a medley of vibrant ingredients including juicy San Marzano tomatoes, creamy Agerola fior di latte, and spicy ventricina. Pure pizza perfection!', 'Molise', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/snei5hmtaen0dhgb2ys1', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (12.99, 'Indulge in the flavors of Campania Pizza: Creamy Agerola fior di latte, zucchini cream, delicate zucchini blossoms, zucchini slices, and tantalizing Grana Padano shavings. A symphony of Italian delights!', 'Campania', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/fjiake4gk1acc5n9bm3s', '2023-06-22 12:14');
-INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (13.99, 'Embrace the taste of Liguria Pizza: Agerola fior di latte, sweet confit yellow and red cherry tomatoes, and fragrant basil pesto. A culinary voyage to the enchanting region of Liguria!', 'Liguria', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/bbg7nyf6qisnukgm9coq', '2023-06-22 12:14';
+INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (13.99, 'Embrace the taste of Liguria Pizza: Agerola fior di latte, sweet confit yellow and red cherry tomatoes, and fragrant basil pesto. A culinary voyage to the enchanting region of Liguria!', 'Liguria', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/bbg7nyf6qisnukgm9coq', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (13.99, 'Experience the essence of Emilia Romagna Pizza: Agerola fior di latte, succulent mortadella, and crunchy pistachio crumble. A delicious tribute to the culinary heritage of Emilia Romagna!', 'Emilia Romagna', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/vzn1gtd48ntpzv8wtscy', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (13.99, 'Discover the flavors of Lazio Pizza: Smoky Agerola smoked provola, savory Ariccia porchetta, and bitter cicoria. A tantalizing taste of the vibrant culinary traditions of Lazio!', 'Lazio', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/kjlvqskstut0myibcqj2', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (13.99, 'Savor the essence of Calabria Pizza: Juicy San Marzano tomatoes, creamy Agerola fior di latte, spicy Nduja di Spilinga, and flavorful caciocavallo cheese. A fiery tribute to the bold flavors of Calabria!', 'Calabria', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/xeh8ipghn4utat2vrg1u', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (13.99, 'Delight in Lombardia Pizza: Creamy Agerola fior di latte, succulent bresaola, fresh red cherry tomatoes, peppery arugula, and savory Grana Padano shavings. A harmonious symphony of flavors inspired by Lombardian cuisine!', 'Lombardia', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/o3feehuan9tuq7kizzi3', '2023-06-22 12:14');
 INSERT INTO `pizzas` (price, description, name, url_pic, created_at) VALUES (13.99, 'Embark on a culinary journey to Trentino with this enticing pizza: Agerola fior di latte, smoky speck, creamy buffalo ricotta, and delicate zucchini blossoms. A delightful blend of flavors inspired by the enchanting region of Trentino!', 'Trentino', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/mqg2xcbdupwnuhiw7a5l', '2023-06-22 12:14');
+<!-- INGREDIENTS
 INSERT INTO `ingredients` (name) VALUES('Pomodori pelati');
 INSERT INTO `ingredients` (name) VALUES('Pomodori datterini rossi confit');
 INSERT INTO `ingredients` (name) VALUES('Pomodori datterini gialli confit');
@@ -40,19 +42,63 @@ INSERT INTO `ingredients` (name) VALUES('Olive denocciolate');
 INSERT INTO `ingredients` (name) VALUES('Capperi');
 INSERT INTO `ingredients` (name) VALUES('Origano');
 INSERT INTO `ingredients` (name) VALUES('Aglio');
-
-
-
-
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (5.99, 'Pomodori pelati, spolverata di formaggio grattugiato e fior di latte di Agerola ', 'Margherita', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/dzkieoorh0hh2ktj8ob5', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (6.99, 'Pomodori pelati, datterini rossi confit, alici, olive denocciolate, capperi, origano e aglio ', 'Marinara Strange', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/gkqt2nhoeejvalrdjdxo', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (7.99, 'Pomodori pelati, spolverata di formaggio grattugiato e mozzarella di bufala', 'Bufalina', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/w0jq0uuu2pzl4jkory49', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (10.99, 'Pomodori pelati, fior di latte di Agerola e alici', 'Napoli', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/rhi77c98l26rxamk4mju', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (11.99, 'Pomodori pelati, fior di latte di Agerola, ventricina piccante', 'Molise', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/snei5hmtaen0dhgb2ys1', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (12.99, 'Fior di latte di Agerola, crema di zucchine, fiori di zucca, zucchine e scaglie di Grana Padano', 'Campania', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/fjiake4gk1acc5n9bm3s', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (13.99, 'Fior di latte di Agerola, datterini gialli e rossi confit e pesto di basilico', 'Liguria', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/bbg7nyf6qisnukgm9coq', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (13.99, 'Fior di latte di Agerola, mortadella e granella di pistacchio', 'Emilia Romagna', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/vzn1gtd48ntpzv8wtscy', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (13.99, 'Provola affumicata di Agerola, porchetta di Ariccia e cicoria', 'Lazio', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/kjlvqskstut0myibcqj2', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (13.99, 'Pomodori pelati, fior di latte di Agerola, Nduja di Spilinga e caciocavallo', 'Calabria', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/xeh8ipghn4utat2vrg1u', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (13.99, 'Fior di latte di Agerola, bresaola, datterini rossi freschi, rucola e scaglie di Grana Padano', 'Lombardia', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/o3feehuan9tuq7kizzi3', '2023-06-22 12:14', false);
-<!--INSERT INTO `pizzas` (price, description, name, url_pic, created_at, deleted) VALUES (13.99, 'Fior di latte di Agerola, speck, ricotta di bufala e fiori di zucca', 'Trentino', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/mqg2xcbdupwnuhiw7a5l', '2023-06-22 12:14', false);
+<!-- TABELLA PONTE PIZZA-INGREDIENTS
+<!-- MARGHERITA
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(1,1);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(1,10);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(1,6);
+<!-- MARINARA STRANGE
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(2,1);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(2,2);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(2,13);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(2,27);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(2,28);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(2,29);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(2,30);
+<!-- BUFALINA
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(3,1);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(3,10);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(3,7);
+<!-- NAPOLI
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(4,1);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(4,6);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(4,13);
+<!-- MOLISE
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(5,1);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(5,6);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(5,14);
+<!-- CAMPANIA
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(6,6);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(6,21);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(6,24);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(6,26);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(6,11);
+<!-- LIGURIA
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(7,6);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(7,2);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(7,3);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(7,20);
+<!-- EMILIA ROMAGNA
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(8,6);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(8,17);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(8,25);
+<!-- LAZIO
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(9,9);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(9,18);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(9,22);
+<!-- CALABRIA
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(10,1);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(10,6);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(10,19);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(10,12);
+<!-- LOMBARDIA
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(11,6);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(11,15);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(11,4);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(11,23);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(11,11);
+<!-- TRENTINO
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(12,6);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(12,16);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(12,8);
+INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES(12,24);
